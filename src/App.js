b2fa578b44c90c6853d6c506476dc1a8cde04820
Alex Dymov <a8dymov@gmail.com>
2020-05-02 21:38:04 +0500

    reworked handleResize function to calculate vh

--- src/App.js
+++ src/App.js
@@ -45,14 +45,6 @@ class App extends React.Component {
         this.setState({ currentUser: userAuth });
       }
     });
-
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-    window.addEventListener('resize', () => {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
   }
 
   componentWillUnmount() {
--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ import App from './App';
 
 import './index.css';
 
+window.onload = () => {
+  const handleResize = () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+
+  window.addEventListener('resize', handleResize);
+  handleResize();
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
